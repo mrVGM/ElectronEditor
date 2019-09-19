9c2e7f8a9fdc801a0de0189e772be22e5599c6f3
Vasko <magaranov@abv.bg>
2019-09-19 15:51:43 +0300

    Icon for game objects

--- core/Assets/Scripts/Hierarchy/gameObject.js
+++ core/Assets/Scripts/Hierarchy/gameObject.js
@@ -2,7 +2,36 @@ let gameObject = {
     extendsFrom: 'Assets\\Scripts\\renderEJS.js',
     createInstance: function() {
         let inst = {
-            name: 'GameObject renderer'
+            name: 'GameObject renderer',
+            params: {
+                icon: {
+                    name: 'Icon',
+                    type: 'gameObject',
+                    value: undefined
+                },
+                name: {
+                    name: 'Name',
+                    type: 'gameObject',
+                    value: undefined
+                }
+            },
+            interface: {
+                refreshLabel: function(inst) {
+                    let icon = inst.params.icon.gameObjectRef;
+                    icon = document.appData.api.getComponent(icon, document.appData.scripts.renderEJS);
+                    let name = inst.params.name.gameObjectRef;
+                    name = document.appData.api.getComponent(name, document.appData.scripts.renderEJS);
+
+                    let iconHTML = icon.interface.findHTMLElement(icon);
+                    let nameHTML = name.interface.findHTMLElement(name);
+
+                    let iconParent = iconHTML.parentElement;
+                    let nameParent = nameHTML.parentElement;
+
+                    iconParent.innerHTML = icon.interface.render(icon);
+                    nameParent.innerHTML = name.interface.render(name);
+                }
+            }
         };
         return inst;
     }
