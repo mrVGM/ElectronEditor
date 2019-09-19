d5f740c3d69f15446647a02f5ca029fc9ad546b5
Vasko <magaranov@abv.bg>
2019-09-19 14:58:19 +0300

    Rendering game objects

--- core/Assets/Scripts/Hierarchy/hierarchy.js
+++ core/Assets/Scripts/Hierarchy/hierarchy.js
@@ -4,13 +4,30 @@ let hierarchy = {
         let inst = {
             name: 'Hierarchy renderer',
             params: {
-                rootGameObject: {
+                gameObjectPrefab: {
                     name: 'Root Game Object',
-                    type: 'gameObject',
+                    type: 'fileObject',
+                    value: undefined
+                },
+                contextHierarchyTag: {
+                    name: 'Context Tag',
+                    type: 'fileObject',
                     value: undefined
                 }
             },
             interface: {
+                getRootGameObject: function(inst) {
+                    let rootGameObject = document.appData.UIContext[inst.params.contextHierarchyTag.value];
+                    if (!rootGameObject) {
+                        let prefabStr = document.appData.library[inst.params.gameObjectPrefab.value].prefabStr;
+                        rootGameObject = document.appData.api.instantiatePrefabFromString(prefabStr);
+                        rootGameObject = document.appData.api.getComponent(rootGameObject, document.appData.scripts.renderEJS);
+                        document.appData.UIContext[inst.params.contextHierarchyTag.value] = rootGameObject;
+                    }
+                    inst.gameObject.children = [rootGameObject.gameObject];
+                    rootGameObject.gameObject.parent = inst.gameObject;
+                    return rootGameObject;
+                }
             }
         };
         return inst;
